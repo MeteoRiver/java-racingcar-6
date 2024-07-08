83a13b118324ed25f2a957e6c951a3e0fe7d6727
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-07-08 11:25:09 +0900

    Add files via upload

---
+++ src/main/java/racingcar/racingcar/Application.java
@@ -0,0 +1,13 @@
+package racingcar;
+import Controller.RacingCar;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+
+            RacingCar racingCar = new RacingCar();
+        racingCar.run();
+    }
+
+}
